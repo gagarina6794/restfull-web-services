ec289dbc720eaf4f029e056ceea5f3514d773828
irhry@smile.fr <iryna6794@gmail.com>
2019-09-19 12:41:56 +0300

    add i18n Internationalizing

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/config/LocaleConfiguration.java
@@ -0,0 +1,26 @@
+package com.in28minutes.rest.webservices.restfulwebservices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfiguration {
+    @Bean
+    public LocaleResolver localResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean(name="messageSource")
+    public ResourceBundleMessageSource bundleMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+}
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,16 +1,31 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controller;
 
 import com.in28minutes.rest.webservices.restfulwebservices.model.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello world";
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public ResponseEntity<String> helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        final String message = messageSource.getMessage("good.morning.message", null, locale);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @GetMapping("/hello-world-bean")
